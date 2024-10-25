351b7b311def0cfd653aacdeaa4630676f3a09e5
Alejandro Cesar Chipana Apaza <alejandrochipanaapaza@gmail.com>
2024-10-25 13:44:02 -0500

    Add files via upload

---
+++ src/semana11/SEMANA11.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana11;
+
+import Pantallas.Pantalla;
+/**
+ *
+ * @author Estudiante
+ */
+public class SEMANA11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Pantalla ventana= new Pantalla();
+    ventana.setLocationRelativeTo(null);
+    ventana.setVisible(true);
+    
+    }
+    
+}
